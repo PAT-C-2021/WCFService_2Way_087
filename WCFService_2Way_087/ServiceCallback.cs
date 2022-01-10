ca96e52683cf06594dfaae8edd5322d696a10c2d
DESKTOP-ND6HMKD\Fanky <andryfanky3@gmail.com>
2022-01-11 00:06:52 +0700

    Change AppConfig and ServiceCallback

--- WCFService_2Way_087/ServiceCallback.cs
+++ WCFService_2Way_087/ServiceCallback.cs
@@ -7,6 +7,7 @@ using System.Text;
 
 namespace WCFService_2Way_087
 {
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class ServiceCallback : IServiceCallback
     {
